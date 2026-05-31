f1a54edef505ae34af9e8785c36cf0c9061b8f7f
Mitrofan <mityirg09@mail.ru>
2026-05-31 14:15:17 -0400

    Copy paste struct point, DelList, DelElByHead from homework

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
+struct point
+{
+  int x, y;
+  struct point* next;
+};
+
 int main()
 {
     printf("Hello World!\n");
     return 0;
+}
+
+int DelElByHead(struct point** list)
+{
+  if(!list) return -1;
+  if(!*list) return 1;
+
+  struct point* ptrIx = *list;
+  *list = (*list)->next;
+  free(ptrIx);
+
+  return 0;
+}
+
+int DelList(struct point** list)
+{
+  if(!list) return -1;
+  if(!*list) return 1;
+
+  struct point* ptrIx = NULL;
+  while(*list)
+  {
+    ptrIx = *list;
+    *list = (*list)->next;
+    free(ptrIx);
+  }
+
+  return 0;
 }
